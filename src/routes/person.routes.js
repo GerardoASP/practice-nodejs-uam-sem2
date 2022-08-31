88ff6d4176fc2090c335f403bf68f5e6b9f10b9a
GerardoASP <gerardo.sanchezp@autonoma.edu.co>
2022-08-31 09:26:24 -0500

    Adding modifications to the methods POST,GET file:person.routes.js

--- src/routes/person.routes.js
+++ src/routes/person.routes.js
@@ -12,8 +12,21 @@ const person_routes = express.Router()
 */
 //:(parametro)
 
-person_routes.post('/',(req,res)=>{})
-person_routes.get('/',(req,res)=>{})//listar
+person_routes.post('/',(req,res)=>{
+	const new_person = person_model(req.body);
+	new_person
+		.save()
+		.then((data) => res.json(data))
+		.catch((err) => res.json(err));
+});
+
+person_routes.get('/',(req,res)=>{
+	person_model
+	 .find()
+	 .then((data) => res.json(data))
+	.catch((err) => res.json(err));
+});
+
 person_routes.get('/:personId',(req,res)=>{})//mirar 
 person_routes.put('/:personId',(req,res)=>{}) //actualizar
 person_routes.delete('/:personId',(req,res)=>{}) //eliminar
